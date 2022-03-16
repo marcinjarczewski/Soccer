1027665790717cb2295a43c3dc97f99de8daa60b
Marcin Jarczewski <marcin.jarczewski@brilliancy.pl>
2022-03-16 07:12:13 +0100

    add and remove goal in ongoing state

---
+++ Brilliancy.Soccer.Common/Dtos/Match/MatchOngoingEditDto.cs
@@ -0,0 +1,16 @@
+﻿using Brilliancy.Soccer.Common.Dtos.Team;
+using Brilliancy.Soccer.Common.Dtos.User;
+using System;
+using System.Collections.Generic;
+
+namespace Brilliancy.Soccer.Common.Dtos.Match
+{
+    public class MatchOngoingEditDto
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public GoalDto Goal { get; set; }
+    }
+}
