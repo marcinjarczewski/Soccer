6ecaccf72141afee3e992e2f31c6e0c0595c7f6d
Marcin Jarczewski <marcin.jarczewski@brilliancy.pl>
2022-03-25 12:43:13 +0100

    remove test site add brand font fix lost password

--- Brilliancy.Soccer.WebApi/wwwroot/js/viewScripts/login/lostPassword.js
+++ Brilliancy.Soccer.WebApi/wwwroot/js/viewScripts/login/lostPassword.js
@@ -2,42 +2,42 @@ define(['knockoutWithAddons', 'messageQueue', 'globalModel', 'helpers', 'loginRe
     function (ko, messageQueue, globalModel, helpers, loginRepository, translations) {
         var ViewModel = function (options) {
             function send() {
-            var validation = ko.validation.group(vm, { deep: true });
-            if (validation().length > 0) {
-                validation().showAllMessages();
-                return false;
-            }
-            if (vm.isBusy()) {
-                return false;
-            }
-            vm.globalModel.spinner(true);
-            vm.isBusy(true);
-                var data = {
-                    email:vm.email()
-                    };
-            var callback = function (result) {
-                vm.globalModel.spinner(false);
-                vm.isBusy(false);
-                if (!result.isSuccess) {
-                    helpers.log(result.message, 'error');
+                if (vm.validationErrors().length > 0) {
+                    vm.validationErrors.showAllMessages();
                     return false;
                 }
-                else {
-                    helpers.log(result.message, 'success');
+                if (vm.isBusy()) {
+                    return false;
                 }
-                return true;
+                vm.globalModel.spinner(true);
+                vm.isBusy(true);
+                var data = {
+                    email: vm.email()
+                };
+                var callback = function (result) {
+                    vm.globalModel.spinner(false);
+                    vm.isBusy(false);
+                    if (!result.isSuccess) {
+                        helpers.log(result.message, 'error');
+                        return false;
+                    }
+                    else {
+                        helpers.log(result.message, 'success');
+                    }
+                    return true;
+                };
+                return loginRepository.lostPassword(data, callback);
+            }
+            var vm = {
+                globalModel: globalModel(),
+                isBusy: ko.observable(false),
+                email: ko.observable().extend({ required: { message: translations.validation.fieldEmpty }, email: { message: translations.validation.email } }),
+                translations: translations.loginLostPassword,
+                send: send
             };
-            return loginRepository.lostPassword(data, callback);
-        }
-        var vm = {
-            globalModel: globalModel(),
-            isBusy: ko.observable(false),
-            email: ko.observable("").extend({ required: { message: translations.validation.fieldEmpty } }),
-            translations: translations.loginLostPassword,
-            send: send
-        };
 
-        return vm;
-    }
-    return ViewModel;
-});
+            vm.validationErrors = ko.validation.group([vm.email]);
+            return vm;
+        }
+        return ViewModel;
+    });
--- Brilliancy.Soccer.WebApi/wwwroot/js/viewScripts/login/test.js
+++
@@ -1,8 +0,0 @@
-define(['jquery', 'helpers', 'storageHelper', 'messageQueue', 'loginModel', 'knockoutPaged', 'globalModel'],
-    function ($, helpers, storageHelper, messageQueue, loginModel, paged, globalModel) {
-        messageQueue.addMessage('działa', 'success');
-        //messageQueue.viewAllMessages();
-        return { globalModel: globalModel(), test: 'aa' };
-    });
-
-//alert();
